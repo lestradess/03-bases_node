8cf7914d3d0b23a218a211af4eab92c45d581be8
Lestrades_ubu <Lestrades@gmail.com>
2022-10-18 17:00:07 +0200

    Guardar tablas en archivo

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const fs = require("fs");
 
-console.clear();// para limiar consola
-console.log(***********************************);
-console.log(    Tabla de multiplicar  )
-console.log(***********************************);
-const base = 5;
+console.clear(); // para limiar consola
+console.log("***********************************");
+console.log("       Tabla de multiplicar  ");
+console.log("***********************************");
+
+const base = 3;
+let salida = "";
 
 for (let i = 1; i <= 10; i++) {
   console.log(`${base} x ${i} = ${base * i}`);
-  
+  salida += `${base} x ${i} = ${base * i}\n`;
 }
-
+fs.writeFileSync(`tabla-${base}.txt`, salida, (err) => {
+  if (err) throw err;
+  console.log(`Tabla-${base}.txt creado`);
+});
