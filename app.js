e1cd983e4b6022dc7ef1f248dcdc03ccf353cc11
Lestrades_ubu <Lestrades@gmail.com>
2022-10-18 16:47:37 +0200

    Inicio

---
+++ app.js
@@ -0,0 +1,12 @@
+
+console.clear();// para limiar consola
+console.log(***********************************);
+console.log(    Tabla de multiplicar  )
+console.log(***********************************);
+const base = 5;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${base} x ${i} = ${base * i}`);
+  
+}
+
